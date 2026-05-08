41590cf2c412c940c5b07689bfafdbbb44c77745
アラトックス <160970328+Aratox-Official@users.noreply.github.com>
2026-05-07 22:03:32 -0400

    Create scramjet.config.js

---
+++ public/scramjet.config.js
@@ -0,0 +1,11 @@
+self.scramjet_config = {
+  prefix: "/scramjet/",
+  bare: "/bare/",
+  files: {
+    wasm:   "/scramjet.wasm.js",
+    worker: "/scramjet.worker.js",
+    client: "/scramjet.client.js",
+    shared: "/scramjet.shared.js",
+    sync:   "/scramjet.sync.js",
+  }
+};
